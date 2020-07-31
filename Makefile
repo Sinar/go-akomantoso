28791e8ac2a0f145d2c2cf10e1163b0038ff70e7
Michael Leow <leow@sinarproject.org>
2020-07-31 19:10:43 +0800

    Summary: Implemented extract Section for ParliamentDebate + pass tests

--- Makefile
+++ Makefile
@@ -5,4 +5,7 @@ build:
 	@go build -o go-akn .
 
 test:
+	@cd  internal/parliament && gotest -v -run ^Test_extractSectionMarkers$
+
+testsa:
 	@cd  internal/state-assembly && gotest -v -run ^Test_extractSectionMarkers$
--- internal/parliament/sections.go
+++ internal/parliament/sections.go
@@ -17,8 +17,50 @@ type SectionMarkers struct {
 }
 
 func hasDatePageMarker(line string) bool {
-	// Look out for day Parliaments sit; isnin|selasa|rabu|khamis|jumaat
-	matched, err := regexp.MatchString(`isnin|selasa|rabu|khamis|jumaat`, strings.ToLower(line))
+	// As per: https://stackoverflow.com/a/42251527
+	normalizedLine := strings.Join(strings.Fields(strings.ToLower(line)), " ")
+	// Page Boundary: DR.18.5.2020 1
+	// could  be DR. 28.7.2020 1 as well
+	// can be as simple as `^dr\.\s*\d+`
+	matched, err := regexp.MatchString(`^dr\.\s*\d+.*\d+$`, normalizedLine)
+	if err != nil {
+		panic(err)
+	}
+	return matched
+}
+
+func hasParliamentDebateID(line string, parliamentDebateID *string) bool {
+	//  Bil. 1 Isnin      18 Mei 2020
+	// As per: https://stackoverflow.com/a/42251527
+	normalizedLine := strings.Join(strings.Fields(strings.ToLower(line)), " ")
+	matched, err := regexp.MatchString(`isnin|selasa|rabu|khamis|jumaat`, normalizedLine)
+	if err != nil {
+		panic(err)
+	}
+	if matched {
+		// Check it starts with bil
+		bmatched, berr := regexp.MatchString(`bil`, normalizedLine)
+		if berr != nil {
+			panic(berr)
+		}
+		if bmatched {
+			// Just naive matching + replace
+			normalizedLine = strings.ReplaceAll(normalizedLine, ".", "")
+			normalizedLine = strings.ReplaceAll(normalizedLine, " ", "-")
+			// Bring out the final transformation back
+			*parliamentDebateID = normalizedLine
+		}
+	}
+	// Replace non alphanum with ''
+	// Replace :space: with  '-'
+	return false
+}
+
+func hasParliamentDebateDate(line string) bool {
+	// Isnin, 18 Mei 2020
+	// As per: https://stackoverflow.com/a/42251527
+	normalizedLine := strings.Join(strings.Fields(strings.ToLower(line)), " ")
+	matched, err := regexp.MatchString(`isnin|selasa|rabu|khamis|jumaat`, normalizedLine)
 	if err != nil {
 		panic(err)
 	}
@@ -26,8 +68,10 @@ func hasDatePageMarker(line string) bool {
 }
 
 func hasSessionStartMarkerLine(line string) bool {
+	// As per: https://stackoverflow.com/a/42251527
+	normalizedLine := strings.Join(strings.Fields(strings.ToLower(line)), " ")
 	// Tuan Yang di-Pertua mempengerusikan Mesyuarat
-	matched, err := regexp.MatchString(`tuan yang di-pertua mempengerusikan mesyuarat`, strings.ToLower(line))
+	matched, err := regexp.MatchString(`tuan yang di-pertua mempengerusikan mesyuarat`, normalizedLine)
 	if err != nil {
 		panic(err)
 	}
@@ -35,14 +79,31 @@ func hasSessionStartMarkerLine(line string) bool {
 }
 
 func extractSectionMarkers(allLines []string) SectionMarkers {
-	var datePageMarker string
+	var datePageMarker, parliamentDebateID, parliamentDebateDate string
 	var sessionStartMarkerLinePage int
 	for i, line := range allLines {
 		// DEBUG
 		//fmt.Println("LINE: ", i)
 		normalizedLine := strings.TrimSpace(line)
 		if hasDatePageMarker(normalizedLine) {
-			datePageMarker = normalizedLine
+			// DEBUG
+			//fmt.Println("MATCHED!!!! ==> ", normalizedLine)
+			// Join all but the last segment as that will be the page number
+			// can be a function later  to extract out pages  ,.
+			datePageFields := strings.Fields(normalizedLine)
+			for i, singleField := range datePageFields {
+				if i < len(datePageFields)-1 {
+					datePageMarker = datePageMarker + singleField
+				}
+			}
+			continue
+		}
+		if hasParliamentDebateID(normalizedLine, &parliamentDebateID) {
+			// Found it, store it normalized and move on ..
+			continue
+		}
+		if hasParliamentDebateDate(normalizedLine) {
+			parliamentDebateDate = normalizedLine
 			continue
 		}
 		if hasSessionStartMarkerLine(normalizedLine) {
@@ -50,17 +111,19 @@ func extractSectionMarkers(allLines []string) SectionMarkers {
 			break
 		}
 	}
+	// Extracted out metadata for ParliamentDebate Day
+	parliamentDebate := ParliamentDebate{
+		ID:        parliamentDebateID,
+		Date:      parliamentDebateDate,
+		Attended:  nil,
+		Missed:    nil,
+		QAHansard: akomantoso.QAHansard{},
+	}
 	// DEBUG
 	//spew.Dump(allLines)
 	return SectionMarkers{
-		DatePageMarker: datePageMarker,
-		ParliamentDebate: ParliamentDebate{
-			ID:        "",
-			Date:      "",
-			Attended:  nil,
-			Missed:    nil,
-			QAHansard: akomantoso.QAHansard{},
-		},
+		DatePageMarker:         datePageMarker,
+		ParliamentDebate:       parliamentDebate,
 		SessionStartMarkerLine: sessionStartMarkerLinePage,
 	}
 }
